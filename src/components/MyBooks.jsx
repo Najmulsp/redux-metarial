36d2fdee63749b13d79d6be4bc6921b0dca357af
Najmul <mdnajmulsarker1595@gmail.com>
2024-12-30 21:49:23 +0600

    learning material ui

--- src/components/MyBooks.jsx
+++ src/components/MyBooks.jsx
@@ -1,10 +1,12 @@
 import { useSelector } from "react-redux";
+import { Typography, Button, IconButton, Box, TextField } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const MyBooks = () => {
-  const  books  = useSelector((state) => state.books.books);
+  const books = useSelector((state) => state.books.books);
 
   return (
-    <div>
+    <>
       {books && books?.length > 0 ? (
         <div>
           {books?.map((book) => (
@@ -14,7 +16,36 @@ const MyBooks = () => {
       ) : (
         <p>There is no book available now. Please take a look later</p>
       )}
-    </div>
+      <section>
+                    {/* Typography */}
+        <Typography variant="h3" component={"h1"} sx={{color: "skyblue"}}>
+            I am exploring material ui
+        </Typography>
+
+
+                       {/* Button */}
+        {/* ডকুমেন্ট দেখে বাটন এর কালার, সাইজ, আইকন ওয়ালা বাটন, আইকন বাটন, টগল বাটন, বাটন গ্রুপ ইত্যাদি বসানো যায়*/}
+        <Button variant="text" sx={{margin: "5px"}}>Click Me</Button>
+        <Button variant="contained" sx={{margin: "5px"}}>Contained</Button>
+        <Button variant="contained" color="success" sx={{margin: "5px"}}>Click Me</Button>
+        <Button variant="outlined" sx={{margin: "5px"}}>Outlined</Button>
+        <IconButton aria-label="delete" sx={{margin: "5px"}}>
+  <DeleteIcon />
+</IconButton>
+                            {/* Text Field */}
+                            <Box
+      component="form"
+      sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Name" variant="outlined" />
+      <TextField id="outlined-basic" type="text" variant="standard" placeholder="Email" />
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+    </Box>
+      </section>
+    </>
   );
 };
 
